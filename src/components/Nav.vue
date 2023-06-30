fa35127e8f910f52da544114bb31b6fab1d5c05a
Widdy LOUIS <louisws@outlook.fr>
2023-07-01 01:22:40 +0200

    Update CSS Dynamically

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,4 +1,14 @@
 <script setup>
+  import {reactive} from 'vue'
+
+  const state = reactive({
+    navOpen: false
+  });
+
+  const setNavOpen = () => {
+    state.navOpen = !state.navOpen
+  };
+
 </script>
 
 <template>
@@ -6,14 +16,14 @@
     <div class="nav-container">
         <div class="navbar">
           <div class="logo">CodeurCopieur</div>
-          <div class="menu-toggle">
-            <div class="hamBox">
-              <span class="lineTop"></span>
-              <span class="lineBottom"></span>
+          <div class="menu-toggle" @click="setNavOpen">
+            <div class="hamBox" :class="{'hamBoxOpen' : state.navOpen}">
+              <span class="lineTop" :class="{'spin' : state.navOpen}"></span>
+              <span class="lineBottom" :class="{'spin' : state.navOpen}"></span>
             </div>
           </div>
         </div>
-        <div class="nav-overlay"></div>
+        <div class="nav-overlay" :style="{top: state.navOpen ? '0' : '-100%', transitionDelay: state.navOpen ? '0s' : '0s'}"></div>
     </div>
   </div>
 </template>
