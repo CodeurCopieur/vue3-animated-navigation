9408ca532e64e24da2d249dc3427728153e498dc
Widdy LOUIS <louisws@outlook.fr>
2023-06-30 22:26:55 +0200

    Coding Hamburger Toggle

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -2,5 +2,18 @@
 </script>
 
 <template>
-  <h1>Nav</h1>
+  <div class="nav">
+    <div class="nav-container">
+        <div class="navbar">
+          <div class="logo">CodeurCopieur</div>
+          <div class="menu-toggle">
+            <div class="hamBox">
+              <span class="lineTop"></span>
+              <span class="lineBottom"></span>
+            </div>
+          </div>
+        </div>
+        <div class="nav-overlay"></div>
+    </div>
+  </div>
 </template>
