41d85653ec852edb70ad28bd7481f2d6e2cc88e8
Widdy LOUIS <louisws@outlook.fr>
2023-07-01 21:44:56 +0200

    Using bind to Update CSS

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -28,33 +28,67 @@
           :style="{top: state.navOpen ? '0' : '-100%', transitionDelay: state.navOpen ? '0s' : '0s'}">
           <ul class="nav-links">
             <li class="nav-item">
-              <router-link to="/">Home</router-link>
+              <router-link 
+                to="/" 
+                @click="setNavOpen"
+                :style="{top: state.navOpen ? '0' : '120px', transitionDelay: state.navOpen ? '.8s' : '0s'}"
+                >Home</router-link>
               <div class="nav-item-wrapper"></div>
             </li>
             <li class="nav-item">
-              <router-link to="/">Project</router-link>
+              <router-link 
+                to="/projets" 
+                @click="setNavOpen"
+                :style="{top: state.navOpen ? '0' : '120px', transitionDelay: state.navOpen ? '.9s' : '0s'}"
+                >Projects</router-link>
               <div class="nav-item-wrapper"></div>
             </li>
             <li class="nav-item">
-              <router-link to="/">About</router-link>
+              <router-link 
+                to="/about" 
+                @click="setNavOpen"
+                :style="{top: state.navOpen ? '0' : '120px', transitionDelay: state.navOpen ? '1s' : '0s'}"
+                >About</router-link>
               <div class="nav-item-wrapper"></div>
             </li>
             <li class="nav-item">
-              <router-link to="/">Contact</router-link>
+              <router-link 
+                to="/" 
+                @click="setNavOpen"
+                :style="{top: state.navOpen ? '0' : '120px', transitionDelay: state.navOpen ? '1.1s' : '0s'}"
+                >Contact</router-link>
               <div class="nav-item-wrapper"></div>
             </li>
           </ul>
           <div class="nav-footer">
-            <div class="location">
+            <div 
+              class="location"
+              :style="{
+                bottom: state.navOpen ? '0' : '-20px',
+                opacity: state.navOpen ? '1' : '0',
+                transitionDelay: state.navOpen ? '1.2s' : '0s'
+                }">
               <span>Paris, IDF</span>
             </div>
             <div class="nav-social-media">
               <ul>
                 <li>
-                  <router-link to="/">Twitter</router-link>
+                  <router-link 
+                    to="/"
+                    :style="{
+                      bottom: state.navOpen ? '0' : '-20px',
+                      opacity: state.navOpen ? '1' : '0',
+                      transitionDelay: state.navOpen ? '1.3s' : '0s'
+                      }">Twitter</router-link>
                 </li>
                 <li>
-                  <router-link to="/">Github</router-link>
+                  <router-link 
+                    to="/"
+                    :style="{
+                    bottom: state.navOpen ? '0' : '-20px',
+                    opacity: state.navOpen ? '1' : '0',
+                    transitionDelay: state.navOpen ? '1.4s' : '0s'
+                    }">Github</router-link>
                 </li>
               </ul>
             </div>
